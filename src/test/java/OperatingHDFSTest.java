010ba1515c4b3a8ee0486928c69f07b1555327f0
AlanP <pwalan@foxmail.com>
2017-01-19 15:48:27 +0800

    add some test

--- src/test/java/OperatingHDFSTest.java
+++ src/test/java/OperatingHDFSTest.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 
 /**
  * @author AlanP
- * @Date 2017/1/17
+ * @date 2017/1/17
  */
 public class OperatingHDFSTest {
     public static void main(String[] args) throws IOException {
         OperatingHDFS ofs = new OperatingHDFS();
-        ofs.catFile("/user/alanp/input/test.txt");
+
+        //查看文件内容
+        //ofs.catFile("/user/alanp/input/test.txt");
+
+        //将本地文件拷贝到hdfs上，C:\Users\pwala\Desktop
+        ofs.copyFile("C:\\Users\\pwala\\Desktop\\test3.txt","/user/alanp/input");
+
     }
 }
